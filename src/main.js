36bab7103bf168bb07676c76e66d80c046223225
Greg Minezzi <greogryminezzi@gmail.com>
2022-01-17 13:39:34 +0100

    finishh lesson

--- src/main.js
+++ src/main.js
@@ -3,3 +3,23 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 const promise = welcome();
 
 console.log(promise);
+
+const question = "";
+const tellPromise = tell(question);
+
+
+tellPromise
+  .catch(console.error)
+  .then((fortune) => {
+    console.log(question);
+    console.log(fortune);
+  });
+
+  welcome()
+  .then(console.log) // Logs the result of welcome()
+  .then(tell) // Calls tell, which returns a rejected promise (no question supplied).
+  .then(console.log) // Skipped because tell returned a rejected promise.
+  .catch(console.error) // Logs error from tell() or welcome()
+  .then(goodbye) // Returns promise from goodbye()
+  .then(console.log) // Logs the result from goodbye()
+  .catch(console.error); // Logs error only from goodbye()
